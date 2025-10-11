e435a7a0df5ccc4e0c06df37896ad92e5adb0de5
Graylen <delilhabigelow@gmail.com>
2025-10-11 05:43:35 -0400

    Setting up Navigation and pages, and init of 404 page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ProtectedRoute } from "./components/protected-routes";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { NotFound } from "./pages/not-found";
+import { Home } from "./pages/home";
+
+const Logout = () => {
+  localStorage.clear();
+  return <Navigate to="/Login" />;
+};
+
+const RegisterAndLogout = () => {
+  localStorage.clear();
+  return <Register />;
+};
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
