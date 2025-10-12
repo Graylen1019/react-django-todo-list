c1886eb97f2a4aba7ea50256bbd0a8ee4eba7185
Graylen <delilhabigelow@gmail.com>
2025-10-12 03:33:29 -0400

    form component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,6 +30,7 @@ function App() {
         />
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<NotFound />} />
 
       </Routes>
