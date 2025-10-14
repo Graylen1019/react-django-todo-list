83f6f2ab42df86608969d6a9ae2e3a7e4f5b9d34
Graylen <delilhabigelow@gmail.com>
2025-10-13 23:46:32 -0400

    Started Home page design, and function. functional create, read, and delete functins established.

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,3 +1,56 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 export const Home = () => {
-    return <div>Home</div>
-}
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert("Note deleted!");
+        } else {
+          alert("Failed to delete note.");
+        }
+      })
+      .catch((error) => alert(error));
+    getNotes();
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) {
+          alert("Note Created!");
+        } else {
+          alert("Unable to create note.");
+        }
+      })
+      .catch((error) => alert(error));
+    getNotes();
+  };
+
+  return (
+    <h1>he</h1>
+  );
+};
